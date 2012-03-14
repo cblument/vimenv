2fdcc76e720f3288150632b5bccbd09561e787bb
Chris Blumentritt <cblument@gmail.com>
2012-03-13 23:35:38 -0500

    Changing the directory that vim stuff get checked out to

--- setup-vim.sh
+++ setup-vim.sh
@@ -9,9 +9,9 @@ if [ ! `which git` ] ; then
   aptitude install git
 fi
 
-git clone git://github.com/godlygeek/tabular.git ~/.vim/autoload/tabular
-git clone git://github.com/scrooloose/syntastic.git ~/.vim/autoload/syntastic
-git clone https://github.com/rodjek/vim-puppet ~/.vim/autoload/puppet
+git clone git://github.com/godlygeek/tabular.git ~/.vim/bundle/tabular
+git clone git://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+git clone https://github.com/rodjek/vim-puppet ~/.vim/bundle/puppet
 
 cat <<EOF > ~/.vimrc
 call pathogen#infect()
